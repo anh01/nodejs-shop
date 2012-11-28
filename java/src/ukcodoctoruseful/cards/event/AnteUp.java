3ed3ed940ffe9b74b902d098ddd1556a2f436fd5
Stephen Hobdell <hobdell.stephen@gmail.com>
2012-11-28 18:41:43 +0000

    Starting to flesh out the events

---
+++ java/src/ukcodoctoruseful/cards/event/AnteUp.java
@@ -0,0 +1,12 @@
+package ukcodoctoruseful.cards.event;
+
+/**
+ * event: Call from the Croupier to each Player to Ante up, or place their first bets.
+ * 
+ * @author Stephen.Hobdell
+ * 
+ */
+public class AnteUp
+{
+
+}
