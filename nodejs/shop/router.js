12bb1fe63a53b8261cb24c329f10bc9a46ae189e
Stephen Hobdell <hobdell.stephen@gmail.com>
2013-09-26 12:43:37 -0400

    fixing routing issue

--- nodejs/shop/router.js
+++ nodejs/shop/router.js
@@ -9,7 +9,7 @@ routeTo['/search'] = handlers.products;
 function route(pathname, method, data, response) {
   //fork as a separate process?
   console.log(routeTo);
-  if(routeTo[pathname] === 'function') {
+  if(typeof routeTo[pathname] === 'function') {
     routeTo[pathname](method, data, response);
   } else {
     response.write("Don't know how to route " + pathname);
